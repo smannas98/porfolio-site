de10d9197fcd8f3313f091357e2abafc91753606
smannasa98 <seanmannas02@gmail.com>
2023-10-29 00:32:29 -0400

    fixed css

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
-  slug: string
-
-}
+  slug: string;
+};
 
 const Button = (props: Props) => {
   return (
-    <button className="button">
-      <span className="navButtonSlug">{props.slug}</span>
+    <button
+      className="transform 
+  -skew-x-12 
+  bg-zinc-300 
+  bg-gradient-to-tr 
+  from-zinc-300 
+  to-zinc-500 
+  box-border 
+  border-black 
+  border-2 
+  border-opacity-0 
+  text-black 
+  font-bold 
+  py-3 
+  px-8 
+  rounded"
+    >
+      <span className="transform skew-x-12">{props.slug}</span>
     </button>
-  )
-}
+  );
+};
 
 export default Button;
--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -32,7 +32,15 @@ const TopNav = (buttonProps: Props , imageProps: ImageProps) => {
       </div>
 
       {/* Nav links; separate out as separate button components */}
-      <ul className="navButtonList">
+      <ul
+        className="flex 
+  items-end 
+  pl-10 
+  flex-grow 
+  bg-gradient-to-t 
+  from-zinc-300 
+  to-zinc-500 
+  text-xl">
         {/* <li>
           <button className="button hover:bg-red text-black font-bold py-3 px-8 rounded ml-12">
             <span className="transform skew-x-12">Button</span>
@@ -43,9 +51,7 @@ const TopNav = (buttonProps: Props , imageProps: ImageProps) => {
         {buttonProps.map((button: ButtonProps) => (
           <li key={button.buttonSlug}>
             <Link href={button.buttonUrl}>
-              <Button
-                slug={button.buttonSlug}
-              />
+              <Button slug={button.buttonSlug} />
             </Link>
           </li>
         ))}
